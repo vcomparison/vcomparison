af30df2314a9cd0e19fa0cc0b2588f14638f1227
Nikita Izrailev <izrailev@phystech.edu>
2019-11-16 14:34:53 +0300

    added commenting

--- frontend/src/pages/components/CommentArea/index.js
+++ frontend/src/pages/components/CommentArea/index.js
@@ -1,13 +1,16 @@
 import React, { PureComponent } from "react";
+import nanoid from 'nanoid';
 import {Button, Form, TextArea} from "semantic-ui-react";
+import { getCurrentDate } from "../../../helpers/getCurrentDate";
 import Comment from "../Comment";
 import "./CommentArea.sass";
 
 class CommentArea extends PureComponent {
   state = {
     commentsList: [
-      { id: 1, message: "We need to know how to treat", date: "10.10.2017" },
-      { id: 2, message: "Control our treatment process", date: "11.10.2017" }
+        // use nanoid to generate unique id
+      { id: nanoid(), message: "We need to know how to treat", date: "10.10.2017" },
+      { id: nanoid(), message: "Control our treatment process", date: "11.10.2017" }
     ],
     comment: {
       commentText: ""
@@ -20,17 +23,21 @@ class CommentArea extends PureComponent {
     this.setState({ comment: { ...comment, commentText: value } });
   };
 
-  onSendComment = (e) => {
-    console.log(e);
+  onSendComment = ({ target }) => {
+    const { commentsList, comment: { commentText }} = this.state;
+    this.setState({ commentsList: [...commentsList, { id: nanoid(), message: commentText, date: getCurrentDate() }], comment: { commentText: '' }})
   };
 
   onCommentStart = (e) => {
-    console.log(e);
     this.setState({ isCommentAreaShown: true });
   };
 
+  onCommentCancel = () => {
+    this.setState({ isCommentAreaShown: false, comment: { commentText: '' } });
+  }
+
   render() {
-    const { commentsList, isCommentAreaShown } = this.state;
+    const { commentsList, isCommentAreaShown, comment: { commentText } } = this.state;
     return (
 
             <div className="comment-area">
@@ -41,10 +48,17 @@ class CommentArea extends PureComponent {
                 </div>
                 { isCommentAreaShown ?
                     <Form className="comment-area__form" onSubmit={this.onSendComment}>
-                      <TextArea className="comment-area__textarea" as="textarea" onChange={this.addCommentText}></TextArea>
-                      <button className="comment-area__button" >Send</button>
+                      <TextArea value={commentText} className="comment-area__textarea" as="textarea" onChange={this.addCommentText}></TextArea>
+                        <div>
+                          <button className="comment-area__button--primary">
+                              Send
+                          </button>
+                          <button className="comment-area__button--secondary" onClick={this.onCommentCancel}>
+                              Cancel comment
+                          </button>
+                        </div>
                     </Form>
-                    :  <button className="comment-area__button" onClick={this.onCommentStart}>Leave comment</button>
+                    :  <button className="comment-area__button--primary" onClick={this.onCommentStart}>Leave comment</button>
                 }
             </div>
     );
